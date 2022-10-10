84fe008f60b4a2ec12dfbf0747e14dbfb21c71eb
parkminzoo <beehoyoung@hanmail.net>
2022-10-10 23:18:03 +0900

    script 수정

--- parkminzoo/js/script.js
+++ parkminzoo/js/script.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+
 //horizon scroll
 (function(){
     init();
@@ -138,4 +138,4 @@ window.onload = function (){
   //첫번째 슬라이드 먼저 보이도록 하기
   goToSlide(0); //지금이 첫번째인것을 지정해줘야 첫페이지 화살표가 사라짐
 
-};
+
